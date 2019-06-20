29ebc9bf4bec0f21642d8ed1bc8bf55b762d60e4
laís <cavalcantislais@gmail.com>
2019-06-20 18:56:18 -0300

    primeira parte

---
+++ src/database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose'); mongoose.set('useCreateIndex', true);
+
+mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser:true});
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
